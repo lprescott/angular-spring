4b53267e99e6bc71885a5feae760d93efd09d4e4
Luke Prescott <luke.prescott@presport.us>
2019-10-22 22:06:27 -0400

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 cd angular
-echo "Checking frontend dependencies."
+echo "Checking frontend dependencies:"
 npm install
 
-echo "Building frontend."
+echo "Building frontend:"
 ng build
 
 cd ../spring
-echo "Building and deploying backend to http://localhost:8080/. Press Ctrl+C to stop Tomcat Server."
+echo "Building and deploying backend to http://localhost:8080/ (press Ctrl+C to stop Tomcat Server):"
 mvn spring-boot:run
-echo "Server off."
