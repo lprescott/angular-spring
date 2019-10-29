0b34ef3d931a68c9b9003162e9f3146005c92ad3
Luke Prescott <luke.prescott@presport.us>
2019-10-29 19:49:45 -0400

    Changed API proxy to any resource under /api/

--- spring/src/main/java/com/example/spring/Application.java
+++ spring/src/main/java/com/example/spring/Application.java
@@ -1,5 +1,6 @@
 package com.example.spring;
 
+import com.fasterxml.jackson.databind.util.TypeKey;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +14,18 @@ import java.util.UUID;
 @RestController
 public class Application {
 
-    @RequestMapping("/resource")
+    @RequestMapping("/api/hello_world")
     public Map<String,Object> home() {
         Map<String,Object> model = new HashMap<String,Object>();
         model.put("id", UUID.randomUUID().toString());
         model.put("content", "Hello World");
+
+        for (String name: model.keySet()){
+            String key = name.toString();
+            String value = model.get(name).toString();
+            System.out.println(key + " " + value);
+        }
+
         return model;
     }
 
